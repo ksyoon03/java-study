946fe6df2a003bc2a0fc9ca08f03851e2769536a
ksyoon03 <godwoo09@gmail.com>
2026-01-13 23:07:11 +0900

    new: 추상클래스, 상수 문제

---
+++ src/Jan_13/Challange/_3/Americano.java
@@ -0,0 +1,18 @@
+package Jan_13.Challange._3;
+
+public class Americano extends Drink{
+    int price = 3000;
+
+    public Americano(String name, int size) {
+        super(name, size);
+    }
+
+    @Override
+    void calculatePrice() {
+        int finalPrice = price;
+        if(size == CafeConstants.SIZE_LARGE){
+            finalPrice += 500;
+        }
+        System.out.println("가격: " + finalPrice);
+    }
+}

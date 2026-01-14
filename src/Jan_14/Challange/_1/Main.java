12e6d97fbe27f1ea58779c10a093fc3f5724c3f1
ksyoon03 <godwoo09@gmail.com>
2026-01-15 00:53:41 +0900

    feat: Object 클래스 오버라이딩 문제

---
+++ src/Jan_14/Challange/_1/Main.java
@@ -0,0 +1,10 @@
+package Jan_14.Challange._1;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("자바의 정석", "1111");
+        Book book2 = new Book("자바의 안정석", "1111");
+        System.out.println(book1);
+        System.out.println(book1.equals(book2));
+    }
+}
